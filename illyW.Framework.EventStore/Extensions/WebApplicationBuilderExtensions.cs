8f586289eba1ba993caa6f47ca07e041b6c2184f
William Paciaroni <william.paciaroni97@gmail.com>
2024-04-19 17:03:28 +0200

    fix: changed eventstore extensions method

--- illyW.Framework.EventStore/Extensions/WebApplicationBuilderExtensions.cs
+++ illyW.Framework.EventStore/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,29 +1,41 @@
-﻿using EventStore.Client;
-using illyW.Framework.Core.RepositoryPattern;
-using illyW.Framework.EFCore;
+﻿using System;
+using System.Linq;
+using EventStore.Client;
+using illyW.Framework.EventStore.Persistence;
 using illyW.Framework.EventStore.Rehydrators;
-using Microsoft.EntityFrameworkCore;
+using illyW.Framework.EventStore.Repositories;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace illyW.Framework.EventStore.Extensions
 {
     public static class WebApplicationBuilderExtensions
     {
-        public static IServiceCollection AddEventStore(this IServiceCollection serviceCollection, string connectionString)
+        public static IServiceCollection EnableEventStore<TContext>(this IServiceCollection services, IConfiguration configuration, string? eventStoreConnectionString = null) where TContext : EventStoreDbContext
         {
-            var settings = EventStoreClientSettings.Create(connectionString);
+            eventStoreConnectionString ??= configuration.GetConnectionString("EventStore");
 
-            var client = new EventStoreClient(settings);
+            ArgumentException.ThrowIfNullOrWhiteSpace(eventStoreConnectionString);
 
-            serviceCollection.AddSingleton(client);
-            
-            return serviceCollection;
+            services.AddSingleton(new EventStoreClient(EventStoreClientSettings.Create(eventStoreConnectionString)));
+
+            services.AddScoped<ICheckpointRepository, CheckpointRepository<TContext>>();
+            services.AddScoped<IStreamCheckpointRepository, StreamCheckpointRepository<TContext>>();
+            services.AddScoped<IAllCheckpointRepository, AllCheckpointRepository<TContext>>();
+
+            return services;
         }
 
         public static IServiceCollection AddRehydrator<TRehydrator>(this IServiceCollection serviceCollection)
             where TRehydrator : BaseRehydrator
         {
+            if (serviceCollection.FirstOrDefault(x => x.ServiceType == typeof(EventStoreClient)) == null)
+            {
+                throw new Exception("Enable Event Store before add rehydrator");
+            }
+
             serviceCollection.AddHostedService<TRehydrator>();
+
             return serviceCollection;
         }
 
