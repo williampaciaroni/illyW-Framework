9f02e083c549388c4ea4268d266e0e5b3ad3cac6
William Paciaroni <william.paciaroni97@gmail.com>
2024-04-19 16:49:16 +0200

    feat: added servicecollection methods

---
+++ illyW.Framework.EventStore/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using EventStore.Client;
+using illyW.Framework.Core.RepositoryPattern;
+using illyW.Framework.EFCore;
+using illyW.Framework.EventStore.Rehydrators;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace illyW.Framework.EventStore.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static IServiceCollection AddEventStore(this IServiceCollection serviceCollection, string connectionString)
+        {
+            var settings = EventStoreClientSettings.Create(connectionString);
+
+            var client = new EventStoreClient(settings);
+
+            serviceCollection.AddSingleton(client);
+            
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddRehydrator<TRehydrator>(this IServiceCollection serviceCollection)
+            where TRehydrator : BaseRehydrator
+        {
+            serviceCollection.AddHostedService<TRehydrator>();
+            return serviceCollection;
+        }
+
+    }
+}
